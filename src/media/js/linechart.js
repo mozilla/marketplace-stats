67f31a4db1e1b65be564b0e752f6b2031008f8ba
spasovski <davor@spasovski.net>
2013-09-18 15:27:45 -0700

    fix tooltip position based on previous parent resize

--- src/media/js/linechart.js
+++ src/media/js/linechart.js
@@ -178,8 +178,8 @@ define('linechart', ['log'], function(log) {
                                 tooltip.html('<p class="timeinfo">' +
                                                 formatTime(d.date) +
                                                 '</p>' + '<b>' + lbls.tooltipValue + ':</b> ' + d.count)
-                                    .style('left', (d3.mouse(this)[0] + 92) + 'px')
-                                    .style('top', (d3.mouse(this)[1] + 125) + 'px');
+                                    .style('left', (d3.mouse(this)[0] + 95) + 'px')
+                                    .style('top', (d3.mouse(this)[1] + 130) + 'px');
                             })
                             .on('mouseout', function(d) {
                                 tooltip.transition()
