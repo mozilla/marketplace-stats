ec9a11ac418941e7a96d8bdfbc51de8c4c345622
Matt Basta <me@mattbasta.com>
2013-09-20 12:14:03 -0700

    Update stats routes to not get wonked up
    <3 @spasovski

--- src/media/js/routes.js
+++ src/media/js/routes.js
@@ -2,13 +2,14 @@
 
 // Please leave quotes around keys! They're needed for Space Heater.
 var routes = window.routes = [
+    {'pattern': '^/stats/?$', 'view_name': 'homepage'},
     {'pattern': '^/$', 'view_name': 'homepage'},
 
-    {'pattern': '^/apps\-added\-by\-payment/$', 'view_name': 'apps_added'},
-    {'pattern': '^/total\-developers/$', 'view_name': 'total_developers'},
-    {'pattern': '^/total\-visits/$', 'view_name': 'total_visits'},
-    {'pattern': '^/apps\-installed/$', 'view_name': 'apps_installed'},
-    {'pattern': '^/apps\-by\-type/$', 'view_name': 'apps_by_type'},
+    {'pattern': '^/stats/apps\-added\-by\-payment/$', 'view_name': 'apps_added'},
+    {'pattern': '^/stats/total\-developers/$', 'view_name': 'total_developers'},
+    {'pattern': '^/stats/total\-visits/$', 'view_name': 'total_visits'},
+    {'pattern': '^/stats/apps\-installed/$', 'view_name': 'apps_installed'},
+    {'pattern': '^/stats/apps\-by\-type/$', 'view_name': 'apps_by_type'},
 
     {'pattern': '^/tests$', 'view_name': 'tests'},
     {'pattern': '^/debug$', 'view_name': 'debug'}
