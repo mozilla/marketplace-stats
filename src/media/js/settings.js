9c2347d52534762d12869e457409a1941a67f6a9
spasovski <davor@spasovski.net>
2013-09-19 19:28:31 -0700

    the init module is 'main'

--- src/media/js/settings.js
+++ src/media/js/settings.js
@@ -2,7 +2,7 @@ define('settings', ['l10n', 'settings_local', 'underscore'], function(l10n, sett
     var gettext = l10n.gettext;
 
     return _.defaults(settings_local, {
-        init_module: 'marketplace',
+        init_module: 'main',
         default_locale: 'en-US',
         api_url: 'http://' + window.location.hostname,  // No trailing slash, please.
 
