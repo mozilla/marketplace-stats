b93d8bb5894cdd8273a85e74b4ad7c96af63e086
spasovski <davor@spasovski.net>
2013-09-20 15:23:16 -0700

    xtag race condition test

--- src/media/js/views/apps_added.js
+++ src/media/js/views/apps_added.js
@@ -1,4 +1,5 @@
-define('views/apps_added', ['chartutils', 'l10n'], function(cutils, l10n) {
+define('views/apps_added', ['brick', 'chartutils', 'l10n'],
+       function(brick, cutils, l10n) {
 
     var gettext = l10n.gettext;
 
@@ -6,7 +7,8 @@ define('views/apps_added', ['chartutils', 'l10n'], function(cutils, l10n) {
         var chartTitle = gettext('Apps Added by Payment Type');
 
         builder.start('apps_chart.html', {title: chartTitle}).done(function() {
-            cutils.createChart('apps_added', gettext('Apps'), gettext('Number of Apps'));
+            cutils.createChart('apps_added', gettext('Apps'),
+                               gettext('Number of Apps'));
         });
 
         builder.z('type', 'root');
