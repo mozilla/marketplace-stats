438cc19577447a92af0ecbe5e49990bd80b76b4c
Davor Spasovski <davor@spasovski.net>
2013-11-01 06:38:34 -0700

    add missing regions (bug 927648)

--- src/media/js/settings.js
+++ src/media/js/settings.js
@@ -23,16 +23,25 @@ define('settings', ['l10n', 'settings_local', 'underscore'], function(l10n, sett
 
         tracking_id: 'UA-36116321-6',
 
+        // A list of regions and their L10n mappings.
         REGION_CHOICES_SLUG: {
             'worldwide': gettext('Worldwide'),
+            'ar': gettext('Argentina'),
             'br': gettext('Brazil'),
+            'cn': gettext('China'),
             'co': gettext('Colombia'),
+            'de': gettext('Germany'),
+            'gr': gettext('Greece'),
+            'hu': gettext('Hungary'),
+            'mx': gettext('Mexico'),
             'me': gettext('Montenegro'),
+            'pe': gettext('Peru'),
             'pl': gettext('Poland'),
             'rs': gettext('Serbia'),
             'es': gettext('Spain'),
             'uk': gettext('United Kingdom'),
             'us': gettext('United States'),
+            'uy': gettext('Uruguay'),
             've': gettext('Venezuela')
         },
 
