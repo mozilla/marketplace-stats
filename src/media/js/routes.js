348d3a6c3bfe2c11959fa3e61a6a5b916a04a4d8
Davor Spasovski <davor@spasovski.net>
2013-11-27 12:25:47 -0500

    add non-trailing slash routes

--- src/media/js/routes.js
+++ src/media/js/routes.js
@@ -6,21 +6,33 @@ var routes = window.routes = [
     {'pattern': '^/$', 'view_name': 'homepage'},
 
     {'pattern': '^/stats/apps\-added\-by\-payment/$', 'view_name': 'apps_added'},
+    {'pattern': '^/stats/apps\-added\-by\-payment$', 'view_name': 'apps_added'},
     {'pattern': '^/stats/total\-developers/$', 'view_name': 'total_developers'},
+    {'pattern': '^/stats/total\-developers$', 'view_name': 'total_developers'},
     {'pattern': '^/stats/total\-visits/$', 'view_name': 'total_visits'},
+    {'pattern': '^/stats/total\-visits$', 'view_name': 'total_visits'},
     {'pattern': '^/stats/apps\-installed/$', 'view_name': 'apps_installed'},
+    {'pattern': '^/stats/apps\-installed$', 'view_name': 'apps_installed'},
     {'pattern': '^/stats/apps\-installed\-total/$', 'view_name': 'apps_installed_total'},
+    {'pattern': '^/stats/apps\-installed\-total$', 'view_name': 'apps_installed_total'},
     {'pattern': '^/stats/apps\-by\-type/$', 'view_name': 'apps_by_type'},
+    {'pattern': '^/stats/apps\-by\-type$', 'view_name': 'apps_by_type'},
     {'pattern': '^/stats/apps\-available\-by\-type/$', 'view_name': 'apps_available_by_type'},
+    {'pattern': '^/stats/apps\-available\-by\-type$', 'view_name': 'apps_available_by_type'},
     {'pattern': '^/stats/apps\-available\-by\-payment/$', 'view_name': 'apps_available_by_premium'},
+    {'pattern': '^/stats/apps\-available\-by\-payment$', 'view_name': 'apps_available_by_premium'},
     {'pattern': '^/stats/gross\-revenue/$', 'view_name': 'gross_revenue'},
+    {'pattern': '^/stats/gross\-revenue$', 'view_name': 'gross_revenue'},
 
     // Per app stats:
     {'pattern': '^/stats/app/([^/<>"\']+)$', 'view_name': 'app_dashboard'},
     {'pattern': '^/stats/app/([^/<>"\']+)/$', 'view_name': 'app_dashboard'},
     {'pattern': '^/stats/app/([^/<>"\']+)/installs/$', 'view_name': 'per_app_installs'},
+    {'pattern': '^/stats/app/([^/<>"\']+)/installs$', 'view_name': 'per_app_installs'},
     {'pattern': '^/stats/app/([^/<>"\']+)/visits/$', 'view_name': 'per_app_visits'},
+    {'pattern': '^/stats/app/([^/<>"\']+)/visits$', 'view_name': 'per_app_visits'},
     {'pattern': '^/stats/app/([^/<>"\']+)/revenue/$', 'view_name': 'per_app_revenue'},
+    {'pattern': '^/stats/app/([^/<>"\']+)/revenue$', 'view_name': 'per_app_revenue'},
 
     {'pattern': '^/tests$', 'view_name': 'tests'},
     {'pattern': '^/debug$', 'view_name': 'debug'}
