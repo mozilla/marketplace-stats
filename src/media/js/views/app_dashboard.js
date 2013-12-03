87c06fa76685c96cd05455556d6fd21b7efb0a0d
Davor Spasovski <davor@spasovski.net>
2013-12-03 15:30:33 -0500

    add install totals to dashboard pages

--- src/media/js/views/app_dashboard.js
+++ src/media/js/views/app_dashboard.js
@@ -1,9 +1,18 @@
-define('views/app_dashboard', ['l10n', 'utils'], function(l10n, utils) {
+define('views/app_dashboard', ['l10n', 'requests', 'urls', 'utils'],
+       function(l10n, requests, urls, utils) {
 
     var gettext = l10n.gettext;
+    var api = urls.api.url;
 
     return function(builder, args) {
-        builder.start('app_dashboard.html', {slug: args[0]});
+        builder.start('app_dashboard.html', {slug: args[0]}).done(function() {
+            requests.get(
+                api('per_app_totals', [args[0]])
+            ).done(function(data) {
+                var $installs = $('.total-val.installs');
+                $installs.text($installs.text() + data.installs.total).show();
+            });
+        });
 
         builder.z('type', 'root');
         builder.z('title', gettext('Loading...'));
--- src/media/js/views/homepage.js
+++ src/media/js/views/homepage.js
@@ -1,9 +1,17 @@
-define('views/homepage', ['l10n'], function(l10n) {
+define('views/homepage', ['l10n', 'requests', 'urls'],
+       function(l10n, requests, urls) {
 
     var gettext = l10n.gettext;
+    var api = urls.api.url;
 
     return function(builder) {
-        builder.start('homepage.html');
+        builder.start('homepage.html').done(function() {
+            requests.get(api('global_totals')).done(function(data) {
+                var $installs = $('.total-val.installs');
+                console.log('data found', data);
+                $installs.text($installs.text() + data.installs.total).show();
+            });
+        });
 
         builder.z('type', 'root');
         builder.z('title', '');
