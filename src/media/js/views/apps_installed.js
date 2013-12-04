23e922722f541fe6837dc683d8f835508dc521e3
almossawi <almossawi@gmail.com>
2013-12-04 12:54:11 -0800

    Superficial changes to UI, first pass

--- src/media/js/views/apps_installed.js
+++ src/media/js/views/apps_installed.js
@@ -6,7 +6,7 @@ define('views/apps_installed', ['chartutils', 'l10n'], function(cutils, l10n) {
     var enableRegions = false;
 
     return function(builder) {
-        var chartTitle = gettext('Apps Installed');
+        var chartTitle = gettext('Apps Installed by Region');
         var context = {title: chartTitle};
         if (enableRegions) context.regions = cutils.regions;
 
