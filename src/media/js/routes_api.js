177a167792572307771da86d23ab2776574c0a2e
Kevin Ngo <ngoke@mozilla.com>
2014-10-02 11:46:59 -0700

    add fxa login route (bug 1073420)

--- src/media/js/routes_api.js
+++ src/media/js/routes_api.js
@@ -27,6 +27,7 @@ define('routes_api', [], function() {
         // App lookup:
         'app': '/api/v1/apps/app/{0}/',
 
+        'fxa-login': '/api/v2/account/fxa-login/',
         'login': '/api/v2/account/login/',
         'logout': '/api/v2/account/logout/',
         'consumer_info': '/api/v2/fireplace/consumer-info/',
