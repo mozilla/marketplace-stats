956f5947c6476958acf96dc3499a996d85aa4dd4
Kevin Ngo <ngoke@mozilla.com>
2014-10-16 10:38:49 -0700

    upstream site config

--- src/media/js/commonplace/site_config.js
+++ src/media/js/commonplace/site_config.js
@@ -2,15 +2,19 @@
     Get Zamboni site configuration data, including waffle switches and FXA
     info.
 */
-define('site_config', ['defer', 'requests', 'settings', 'urls'],
-    function(defer, requests, settings, urls) {
+define('site_config', ['defer', 'requests', 'settings', 'underscore', 'urls'],
+    function(defer, requests, settings, _, urls) {
 
     function fetch() {
         var def = defer.Deferred();
-        requests.get(urls.api.url('site-config')).done(function(data) {
-            settings.fxa_auth_url = data.fxa.fxa_auth_url;
-            settings.fxa_auth_state = data.fxa.fxa_auth_state;
-            settings.switches = data.waffle.switches || [];
+        requests.get(urls.api.unsigned.url('site-config')).done(function(data) {
+            if (data.hasOwnProperty('fxa')) {
+                settings.fxa_auth_url = data.fxa.fxa_auth_url;
+                settings.fxa_auth_state = data.fxa.fxa_auth_state;
+            }
+            if (data.waffle.switches && _.isArray(data.waffle.switches)) {
+                settings.switches = data.waffle.switches;
+            }
             def.resolve(data);
         }).always(function() {
             def.resolve();
