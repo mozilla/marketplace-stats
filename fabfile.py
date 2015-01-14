b4614cf7adbbff8ed44c276948c03059e818a8ec
Jeremiah Orem <oremj@oremj.com>
2015-01-14 11:19:42 -0800

    fabfile: add jenkins deploy targets

--- fabfile.py
+++ fabfile.py
@@ -33,6 +33,30 @@ def update():
 
 
 @task
+def build():
+    with lcd(MARKETPLACE_STATS):
+        local('npm install')
+        local('make install')
+        local('cp src/media/js/settings_local_hosted.js '
+              'src/media/js/settings_local.js')
+        local('make build')
+        local('node_modules/.bin/commonplace langpacks')
+
+
+@task
+def deploy_jenkins():
+    rpmbuild = helpers.build_rpm(name='marketplace-stats',
+                                 env=settings.ENV,
+                                 cluster=settings.CLUSTER,
+                                 domain=settings.DOMAIN,
+                                 root=ROOT,
+                                 package_dirs=['marketplace-stats'])
+
+    rpmbuild.local_install()
+    rpmbuild.remote_install(['web'])
+
+
+@task
 @roles('web')
 @parallel
 def _install_package(rpmbuild):
